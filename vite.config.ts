3448cf396f753954a67bdb411a724f4f6b090f91
pestypig <you@example.com>
2025-11-04 16:41:27 -0500

    Tweak build configs

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,7 @@ export default defineConfig({
       "@": path.resolve(import.meta.dirname, "client", "src"),
       "@shared": path.resolve(import.meta.dirname, "shared"),
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      idb: path.resolve(import.meta.dirname, "node_modules", "idb", "build", "index.js"),
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
